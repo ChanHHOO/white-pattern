3655ef40564adf80bf4403ab007817eb2b050799
junha-ahn <junha.ahn.dev@gmail.com>
2024-08-24 20:19:40 +0900

    refactor: getChangeTags

--- src/coupang/main-page.js
+++ src/coupang/main-page.js
@@ -2,27 +2,7 @@ console.log('js script loaded');
 
 onLoad(function () {
   console.log('onLoaded');
-
-  var domChanger = new DomChanger();
-
-  // domChanger.preserveStructurePath = [
-  //   "#container",
-  //   "#full-gnb-header",
-  //   "#header",
-  //   "section",
-  //   [".logo", ".search-form.product-search.clearFix", ".icon-menus"]
-  // ];
-
-  domChanger.updateElementStyles = [
-    ["#header", { height: "1007px" }],
-    [".top-bar", { display: "none !important;" }],
-    ["div.gnb-menu-container", { display: "none !important;" }],
-    ["div.category-btn", { display: "none !important;" }],
-    ["#coupang-banner", { display: "none !important;" }],
-    ["#todaysHot", { display: "none !important;" }],
-    ["#contents", { display: "none !important;" }],
-    ["#footer", { display: "none !important;" }],
-  ]
-
-  domChanger.run();
+  
+  var domChanger = new DomChanger(SITE.COUPANG);
+  domChanger.run(PAGE.MAIN);
 });
--- src/dom-changer.js
+++ src/dom-changer.js
@@ -8,69 +8,97 @@ function onLoad(callback) {
   }, 100);
 }
 
+const SITE = {
+  COUPANG: 'coupang',
+  TEMU: 'temu',
+}
+
+const PAGE = {
+  MAIN: 'main',
+  SEARCH: 'search',
+  PRODUCT: 'product',
+}
+
 class DomChanger {
-  constructor() {
-    this.preserveStructurePath = [];
-    this.updateElementStyles = [];
-    this.removeElements = [];
+  constructor(site) {
+    if (site !== SITE.COUPANG && site !== SITE.TEMU) {
+      throw new Error('Invalid site');
+    }
+    this.site = site;
+    this.updateElements = [];
   }
 
-  run() {
-    console.log('DomChanger.run()');
-    if (this.preserveStructurePath.length > 0) {
-      this.preserveSpecificStructure(this.preserveStructurePath);
-    }
-    if (this.updateElementStyles.length > 0) {
-      for (const [selector, style] of this.updateElementStyles) {
-        this.updateElementStyle(selector, style);
+  getChangeTags(page) {
+    // using API to get the tags info from the LLM-server finding the bad-patterns
+    console.log('DomChanger.getChangeTags()');
+    if (this.site === SITE.COUPANG) {
+      const commonPages = [
+        ["#todaysHot", { display: "none !important;" }],
+        [".top-bar", { display: "none !important;" }],
+        ["div.gnb-menu-container", { display: "none !important;" }],
+        ["div.category-btn", { display: "none !important;" }],
+        ["#coupang-banner", { display: "none !important;" }],
+        ["#footer", { display: "none !important;" }],  
+      ]
+      const commonContensts = [
+        ["#sdp-mid-banner-btf", { display: "none !important;" }],
+          ["#promotionCarousel", { display: "none !important;" }],
+          ["#welcomebackNudage", { display: "none !important;" }],
+          ["#searchCarouselWidget", { display: "none !important;" }],
+          ["#searchAlsoViewedProducts", { display: "none !important;" }],
+          ["div.recommendation", { display: "none !important;" }],
+          ["div#sdpAdsBottom", { display: "none !important;" }],
+          ["div#promotionCarousel", { display: "none !important;" }],
+          ["div#midCarousel", { display: "none !important;" }],
+          ["div#midCarousel1", { display: "none !important;" }],
+          ["div#midCarousel2", { display: "none !important;" }],
+          ["div#midCarousel3", { display: "none !important;" }],
+          ["div#midCarousel4", { display: "none !important;" }],
+          ["div#prod-detail-recommend", { display: "none !important;" }],
+          ["div.sdp-also-viewed-products-widget", { display: "none !important;" }],
+          ["div#peopleAlsoBought", { display: "none !important;" }],
+          ["div#sdpBottomAds", { display: "none !important;" }],
+          ["div#sdp-bottom-banner", { display: "none !important;" }],
+          [".srp-top-banner-container", { display: "none !important;" }],
+          ["#srp-bottom-carousel-dco-contaner", { display: "none !important;" }],
+          ["#srpKeywordProductTopBanner", { display: "none !important;" }],
+          [".sdw-aging", { display: "none !important;" }],
+      ]
+      if (page === PAGE.MAIN) {
+        this.updateElements = [
+          ...commonPages,
+
+          ["#header", { height: "1007px" }],
+          ["#contents", { display: "none !important;" }],
+        ]
+      } else if (page === PAGE.SEARCH) {
+        this.updateElements = [
+          ...commonPages,
+          ...commonContensts,
+        ]
+      } else if (page === PAGE.PRODUCT) {
+        this.updateElements = [
+          ...commonPages,
+          ...commonContensts
+        ]
       }
-    }
-    if (this.removeElements.length > 0) {
-      for (const selector of this.removeElements) {
-        this.removeElement(selector);
-      }
-    }
-  }
-
-  /**
-   * Removes an element by its selector along with its children.
-   * @param {string} selector - The selector of the element to remove.
-   */
-  removeElement(selector) {
-    const elements = document.querySelectorAll(selector);
-    for (const element of elements) {
-      element.remove();
-    }
-  }
-
-  /**
-   * Keeps only the specified structure starting from the root ID.
-   * @param {string} rootId - The ID of the root element to start with.
-   * @param {string[]} structure - The hierarchy of IDs to preserve.
-   */
-  preserveSpecificStructure(path) {
-    let baseEelement = document.body;
-    for (const selectorElement of path) {
-      const sevivorElements = [];
+    } else if (this.site === SITE.TEMU) {
       
-      if (typeof selectorElement === 'string') {
-        sevivorElements.push(...baseEelement.querySelectorAll(selectorElement));
-      }
-      if (selectorElement instanceof Array) {
-        for (const selector of selectorElement) {
-          sevivorElements.push(...baseEelement.querySelectorAll(selector));
-        }
-      }
+    }
+  }
 
-      console.log('sevivorElements: ', sevivorElements)
-      while (baseEelement.firstChild) {
-        baseEelement.removeChild(baseEelement.firstChild);
-      }
-      for (const sevivorElement of sevivorElements) {
-        baseEelement.appendChild(sevivorElement);
-      }
-      if (sevivorElements.length > 0) {
-        baseEelement = sevivorElements[0];
+  run(page) {
+    console.log('DomChanger.run()');
+    this.getChangeTags(page);
+    
+    const HOUR = 60 * 60 * 1000;
+    setInterval(() => {
+      this.getChangeTags(page);
+    }, HOUR);
+
+    if (this.updateElements.length > 0) {
+      for (const [selector, style] of this.updateElements) {
+        this.updateElementStyle(selector, style);
       }
     }
   }
